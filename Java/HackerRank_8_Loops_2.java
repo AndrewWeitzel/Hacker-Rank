0a21091b89815fc8aa12279732a4ec7b956053f8
AndrewWeitzel <44351484+AndrewWeitzel@users.noreply.github.com>
2019-10-11 10:24:32 -0400

    Initial commit

---
+++ Java/HackerRank_8_Loops_2.java
@@ -0,0 +1,51 @@
+/*
+    Objective
+    We use the integers a, b, and n to create the following series:
+        (a + 2^0 + b), (a + 2^0 * b + 2^1 * b),..., (a + 2^0 * b + 2^1 * b + ... + 2^n-1 * b)
+
+
+    You are given q queries in the form of a, b, and n. For each query, print the series 
+    corresponding to the given a, b, and n values as a single line of space-separated integers. 
+
+    Input Format:
+    The first line contains an integer, q, denoting the number of queries.
+    Each line i of q the subsequent lines contains three space-separated integers describing the 
+    respective ai, bi, and ni values for that query. 
+    
+    Constraints:
+    0 ≤ q ≤ 500
+    0 ≤ a,b ≤ 50
+    1 ≤ n ≤ 15
+
+    Output Format:
+    For each query, print the corresponding series on a new line. Each series must be printed in 
+    order as a single line of n space-separated integers.
+
+*/
+
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int output = a + Math.pow(2, 0) * b; 
+            if (i!=0){
+                for(int j = i; j<0;i--){
+                    output+= b + Math.pow(2, i);
+                }
+            }
+            System.out.print(output + " ");
+        }
+        in.close();
+    }
+}
+
+
+
+
